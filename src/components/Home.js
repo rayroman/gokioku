7bb8977dce71e579b6fed3f7645b3f59c1966ed6
Ray Roman <rroman681@ucla.edu>
2017-06-22 11:27:34 -0700

    Style main page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,19 +4,29 @@
  */
 import React, {Component} from "react";
 import {Route, Switch} from "react-router";
+import FaGithub from "react-icons/lib/fa/github";
 import {Link} from "react-router-dom";
+
+// CSS stuff
+import "../stylesheets/Home.css";
 
 // Todo: Make logo
 
 class Home extends Component {
     render() {
         return (
-            <article>
+            <article className="Home">
                 <div>Logo goes here.</div>
                 <ul>
                     <li><Link to="/easy">Easy</Link></li>
                     <li><Link to="/hard">Hard</Link></li>
                 </ul>
+                <div className="git">
+                    <a href="https://github.com/rayroman"
+                       rel="noopener noreferrer"
+                       target="_blank"
+                       alt="GitHub"><FaGithub/></a>
+                </div>
             </article>
         )
     }
