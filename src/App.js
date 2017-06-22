bea80a0ac2466180b4caf5023e21cd87614fb872
Ray Roman <rroman681@ucla.edu>
2017-06-22 12:01:09 -0700

    Create routes for app

--- src/App.js
+++ src/App.js
@@ -3,40 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import {connect} from "react-redux";
 import {fetchCharactersAction} from "./store/actions";
+import {Switch, Route} from "react-router";
+import Home from "./components/Home";
 
 class App extends Component {
-    // function to get the data
-    getData(diff) {
-        this.props.fetchCharacters(diff)
-            .then(() => {
-                console.log(this.props.characters);
-            });
-    }
-
     render() {
         return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2 onClick={e => {e.preventDefault(); this.getData("EASY")}}>Welcome to React</h2>
-                </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-            </div>
-        );
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route path="/:difficulty"
+                       render={({match}) => {
+                           const settings = ["easy", "hard"]; // may add more later
+                           // Todo: add return
+                       }}
+                />
+                {/*Todo: add catch-all route*/}
+            </Switch>
+        )
     }
 }
 
-const mapStateToProps = state => ({
-    difficulty: state.difficulty,
-    characters: state.characters
-});
-
-const mapDispatchToProps = dispatch => ({
-    fetchCharacters(diff) {
-        return dispatch(fetchCharactersAction(diff))
-    }
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
